d719f406ba0de78de9e3a3be66ac465894c8c684
Scott Fleming <fleming@stsci.edu>
2023-08-21 15:23:19 -0400

    first version of ARCES read_spec implemented

--- pytodcor/read_spec_arc35.py
+++ pytodcor/read_spec_arc35.py
@@ -3,7 +3,17 @@
    :synopsis: Reads an APO 3.5m ARC file containing spectroscopic data.
 """
 
+import logging
+import os
+from astropy import units as u
+from astropy.io import fits
+from astropy.coordinates import SkyCoord
+from astropy.time import Time
+import astropy.wcs as apwcs
+from specutils.spectra import Spectrum1D
 from pytodcor.spectrum import Spectrum
+
+logger = logging.getLogger("read_spec_arc35")
 
 def read_spec_arc35(spec_file):
     """
@@ -14,4 +24,44 @@ def read_spec_arc35(spec_file):
 
     :returns: dict -- Spectroscopic data and metadata from the file.
     """
-    return 0
+    if os.path.isfile(spec_file):
+        with fits.open(spec_file) as hdulist:
+            hdr0 = hdulist[0].header
+            dat0 = hdulist[0].data
+        # Extract metadata from the primary header.
+
+        # Target name from the header.
+        objname = hdr0['OBJNAME']
+
+        # Target coordinates.
+        equinox = hdr0['EQUINOX']
+        if equinox == 2000.0:
+            this_equinox = 'J2000'
+        else:
+            logger.error("Did not find a supported value in the EQUINOX header keyword,"
+                         " found value = %s", str(equinox))
+            raise ValueError("Did not find a supported value in the EQUINOX header keyword,"
+                         " found value = %s", str(equinox))
+        this_coord = SkyCoord(ra=hdr0['RA'], dec=hdr0['DEC'], unit=(u.hourangle, u.deg),
+                               frame=hdr0['RADECSYS'].lower(), equinox=this_equinox)
+
+        # Total exposure time in seconds, to calculate time at mid-point of integration.
+        exptime = hdr0['EXPTIME']
+        this_ut_mid = Time(hdr0['UT1'], format='isot', scale='ut1') + (exptime/2.)*u.s
+        this_utc_jd_mid = this_ut_mid.utc.jd
+
+        # Generate a Spectrum1D object.
+        this_wcs = apwcs.WCS(header={'CDELT1': hdr0['CDELT1'], 'CRVAL1': hdr0['CRVAL1'],
+                                      'CUNIT1': 'Angstrom', 'CTYPE1': hdr0['CTYPE1'],
+                                      'CRPIX1': hdr0['CRPIX1']})
+        spec = Spectrum1D(flux=dat0*u.dimensionless_unscaled, wcs=this_wcs)
+
+        # Construct the Spectrum object.
+        this_spec = Spectrum(name=objname, air_or_vac="air", obj_coord=this_coord,
+                             juldate=this_utc_jd_mid, tel_lat=hdr0['LATITUDE'],
+                             tel_long=hdr0['LONGITUD'], exptime=exptime)
+        this_spec.add_spec_part(spec)
+    else:
+        logger.error("File not found: %s", spec_file)
+        raise IOError("File not found: %s", spec_file)
+    return this_spec
--- pytodcor/spectrum.py
+++ pytodcor/spectrum.py
@@ -2,12 +2,20 @@
 .. module:: spectrum
    :synopsis: Defines the Spectrum class to store spectroscopic data.
 """
+
+import logging
+import numpy as np
+from specutils.spectra import Spectrum1D
+
+logger = logging.getLogger("spectrum")
+
 class Spectrum:
     """
     This class defines a Spectrum object to store spectroscopic data and metadata necessary to
     perform cross-correlation and derive radial velocities.
     """
-    def __init__(self):
+    def __init__(self, name="", air_or_vac="", obj_coord=None, juldate=np.nan, bjuldate=np.nan,
+                 tel_lat=np.nan, tel_long=np.nan, exptime=np.nan):
         """
         Class constructor.
         """
@@ -16,25 +24,27 @@ class Spectrum:
         # wavelengths and fluxes that can be cross-correlated indepdently, or individual orders of
         # an Echelle spectrum if the orders have not been stitched into a single spectrum.
         self.parts = []
-        self.name = ""
-        self.air_or_vac = ""
-        self.obj_ra = -999.999
-        self.obj_dec = -999.999
-        self.juldate = -999.999
-        self.bjuldate = -999.999
+        self.name = name
+        if air_or_vac in ["", "air", "vaccuum"]:
+            self.air_or_vac = air_or_vac
+        else:
+            logger.error("air_or_vac argument given unsupported value, was given: %s", air_or_vac)
+        self.obj_coord = obj_coord
+        self.juldate = juldate
+        self.bjuldate = bjuldate
+        self.tel_lat = tel_lat
+        self.tel_long = tel_long
+        self.exptime = exptime
 
-    def add_spec_part(self, wls, fls, flerrs=None):
+    def add_spec_part(self, spec):
         """
-        Adds a new "part" of wavelengths, fluxes, and (optional) flux uncertainties.
+        Adds a new "part" of wavelengths and fluxes via a Spectrum1D object.
 
-        :param wls: The set of wavelengths, in Angstroms, for this part.
-        :type wls: numpy.ndarray
-
-        :param fls: The set of normalized fluxes for this part.
-        :type fls: numpy.ndarray
-
-        :param flerrs: [Optional] The set of flux errors for this part.
-        :type flerrs: numpy.ndarray
+        :param spec: The one-dimensional spectrum for this part.
+        :type wls: specutils.Spectrum1D
         """
-
-        self.parts.append({"wls":wls, "fls":fls, "flerrs":flerrs})
+        if isinstance(spec, Spectrum1D):
+            self.parts.append(spec)
+        else:
+            logger.error("Attempt to add spectrum of unsupported type, must be a specutils"
+                         " Spectrum1D object, was given type %s", str(type(spec)))
